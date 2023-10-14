db21b51c8b9ec86fc1def689c312d99dbe043c58
Ahmad Kayali <kayali1411@gmail.com>
2023-10-14 21:52:40 +0200

    chore(deps): install @svgr/rollup for loading svg files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr({ typescript: true })],
   test: {
     globals: true,
     environment: 'jsdom',
