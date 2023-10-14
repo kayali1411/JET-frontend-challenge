0b8b2b3ff455ba03b348cebe4e966c8592285cc9
Ahmad Kayali <kayali1411@gmail.com>
2023-10-14 21:31:08 +0200

    chore(deps): install and configure eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,20 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: '2021',
+    sourceType: 'module',
+  },
+  plugins: ['react-refresh', 'react', '@typescript-eslint'],
   rules: {},
 }
