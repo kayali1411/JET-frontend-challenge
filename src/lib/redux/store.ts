164a027057a1fe8fe77055e58420b4a24fb3488f
Ahmad Kayali <kayali1411@gmail.com>
2023-10-16 19:40:32 +0200

    chore(test): add render wrapper for testing component with redux

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,22 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {
+  PreloadedState,
+  combineReducers,
+  configureStore,
+} from '@reduxjs/toolkit';
 import playerSlice from './playerSlice';
 import gameStats from './gameStatsSlice';
 import gameApiSlice from './gameApiSlice';
 import errorSlice from './errorSlice';
 
-const store = configureStore({
-  reducer: {
-    player: playerSlice.reducer,
-    gameStats: gameStats.reducer,
-    error: errorSlice.reducer,
-    [gameApiSlice.reducerPath]: gameApiSlice.reducer,
-  },
-  middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(gameApiSlice.middleware);
-  },
+const rootReducer = combineReducers({
+  player: playerSlice.reducer,
+  gameStats: gameStats.reducer,
+  error: errorSlice.reducer,
+  [gameApiSlice.reducerPath]: gameApiSlice.reducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => {
+      return getDefaultMiddleware().concat(gameApiSlice.middleware);
+    },
+    preloadedState,
+  });
+};
+
+const store = setupStore();
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
 
 export default store;
