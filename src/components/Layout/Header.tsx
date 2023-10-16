3eb298e13b1d7da3bc20c45e50bb112dd96fc8fa
Ahmad Kayali <kayali1411@gmail.com>
2023-10-16 09:01:28 +0200

    feat(header): add room name to app header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,13 +1,25 @@
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { useAppSelector } from '../../lib/redux';
 
 const Header: React.FC = () => {
+  const selectedRoom = useAppSelector((state) => state.gameStats.room);
+
   return (
     <header className="flex items-center p-4 bg-primray space-x-4 drop-shadow-md">
       <div className="p-2 bg-primrayDark rounded-full">
         <Logo />
       </div>
       <div>
-        <h1 className="text-2xl text-white font-bold m-0">Game of Three</h1>
+        {!selectedRoom ? (
+          <h1 className="text-2xl text-white font-bold m-0">Game of Three</h1>
+        ) : (
+          <div className="flex flex-col align-start">
+            <h1 className="text-lg text-white font-bold m-0">
+              Playing with {selectedRoom.name}
+            </h1>
+            <span className="text-white">win the game or win job</span>
+          </div>
+        )}
       </div>
     </header>
   );
